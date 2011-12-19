4de0e3335fc8dfa34a1f7368a3808971220cf798
Priit Haamer <priit@fraktal.ee>
2011-12-19 18:37:29 +0200

    Implement contact functions.
    New functions: create_contact, delete_contact, info_contact, list_contact, update_contact

--- lib/epp-eis/contact.rb
+++ lib/epp-eis/contact.rb
@@ -42,6 +42,106 @@ module Epp
       end
     end
     
+    class ContactCreateResponse
+      def initialize(response)
+        @response = Nokogiri::XML(response)
+      end
+      
+      def code
+        @response.css('epp response result').first['code'].to_i
+      end
+
+      def message
+        @response.css('epp response result msg').text
+      end
+      
+      def contact_id
+        @response.css('contact|creData contact|id', 'contact' => XML_NS_CONTACT).text
+      end
+      
+      def contact_create_date
+        @response.css('contact|creData contact|crDate', 'contact' => XML_NS_CONTACT).text
+      end
+    end
+    
+    class ContactDeleteResponse
+      def initialize(response)
+        @response = Nokogiri::XML(response)
+      end
+      
+      def code
+        @response.css('epp response result').first['code'].to_i
+      end
+
+      def message
+        @response.css('epp response result msg').text
+      end
+    end
+    
+    class ContactInfoResponse
+      def initialize(response)
+        @response = Nokogiri::XML(response)
+      end
+      
+      def code
+        @response.css('epp response result').first['code'].to_i
+      end
+
+      def message
+        @response.css('epp response result msg').text
+      end
+      
+      def contact_id
+        @response.css('contact|infData contact|id', 'contact' => XML_NS_CONTACT).text
+      end
+
+      def contact_roid
+        @response.css('contact|infData contact|roid', 'contact' => XML_NS_CONTACT).text
+      end
+
+      def contact_status
+        @response.css('contact|infData contact|status', 'contact' => XML_NS_CONTACT).first['s']
+      end
+
+      def contact_name
+        @response.css('contact|infData contact|postalInfo contact|name', 'contact' => XML_NS_CONTACT).text
+      end
+      
+      def contact_street
+        @response.css('contact|infData contact|addr contact|street', 'contact' => XML_NS_CONTACT).text
+      end
+      
+      def contact_city
+        @response.css('contact|infData contact|addr contact|city', 'contact' => XML_NS_CONTACT).text
+      end
+      
+      def contact_postal_code
+        @response.css('contact|infData contact|addr contact|pc', 'contact' => XML_NS_CONTACT).text
+      end
+      
+      def contact_country_code
+        @response.css('contact|infData contact|addr contact|cc', 'contact' => XML_NS_CONTACT).text
+      end
+      
+      def contact_email
+        @response.css('contact|infData contact|email', 'contact' => XML_NS_CONTACT).text
+      end
+    end
+    
+    class ContactUpdateResponse
+      def initialize(response)
+        @response = Nokogiri::XML(response)
+      end
+      
+      def code
+        @response.css('epp response result').first['code'].to_i
+      end
+
+      def message
+        @response.css('epp response result msg').text
+      end
+    end
+    
     module ContactCommands
       
       # Check availability for a contact id. You may provide multiple contact id names.
@@ -49,7 +149,7 @@ module Epp
         builder = build_epp_request do |xml|
           xml.command {
             xml.check {
-              xml.check('xmlns:contact' => XML_NS_CONTACT, 'xsi:schemaLocation' => 'http://www.nic.cz/xml/epp/domain-1.4.xsd') {
+              xml.check('xmlns:contact' => XML_NS_CONTACT, 'xsi:schemaLocation' => 'http://www.nic.cz/xml/epp/contact-1.6 contact-1.6.xsd') {
                 xml.parent.namespace = xml.parent.namespace_definitions.first
                 contacts.each { |contact| xml.id_ contact }
               }
@@ -61,19 +161,111 @@ module Epp
         ContactCheckResponse.new(request(builder.to_xml))
       end
       
-      def create_contact
+      # Create a new contact object. The contact object will be available immediately.
+      def create_contact(contact, name, street, city, postal_code, country_code, voice, email, ident, ident_type)
+        builder = build_epp_request do |xml|
+          xml.command {
+            xml.create {
+              xml.create('xmlns:contact' => XML_NS_CONTACT, 'xsi:schemaLocation' => 'http://www.nic.cz/xml/epp/contact-1.6 contact-1.6.xsd') {
+                xml.parent.namespace = xml.parent.namespace_definitions.first
+                xml.id_ contact
+                xml.postalInfo {
+                  xml.name name
+                  xml.addr {
+                    xml.street street
+                    xml.city city
+                    xml.pc postal_code
+                    xml.cc country_code
+                  }
+                }
+                xml.voice voice
+                xml.email email
+                xml.ident ident, 'type' => ident_type
+              }
+            }
+            xml.clTRID UUIDTools::UUID.timestamp_create.to_s
+          }
+        end
+        
+        ContactCreateResponse.new(request(builder.to_xml))
       end
       
-      def delete_contact
+      #￼Delete contact handle. Contact object can not be deleted if it has relations to other objects like domains or
+      # nssets.
+      def delete_contact(contact)
+        builder = build_epp_request do |xml|
+          xml.command {
+            xml.delete {
+              xml.delete('xmlns:contact' => XML_NS_CONTACT, 'xsi:schemaLocation' => 'http://www.nic.cz/xml/epp/contact-1.6 contact-1.6.xsd') {
+                xml.parent.namespace = xml.parent.namespace_definitions.first
+                xml.id_ contact
+              }
+            }
+            xml.clTRID UUIDTools::UUID.timestamp_create.to_s
+          }
+        end
+        
+        ContactDeleteResponse.new(request(builder.to_xml))
       end
       
-      def info_contact
+      # Returns detailed information about a contact.
+      def info_contact(contact)
+        builder = build_epp_request do |xml|
+          xml.command {
+            xml.info {
+              xml.info('xmlns:contact' => XML_NS_CONTACT, 'xsi:schemaLocation' => 'http://www.nic.cz/xml/epp/contact-1.6 contact-1.6.xsd') {
+                xml.parent.namespace = xml.parent.namespace_definitions.first
+                xml.id_ contact
+              }
+            }
+            xml.clTRID UUIDTools::UUID.timestamp_create.to_s
+          }
+        end
+        
+        ContactInfoResponse.new(request(builder.to_xml))
       end
       
       def list_contacts
+        list_command('listContacts')
       end
       
-      def update_contact
+      # Update contact object. All domain and nsset objects will be updated as well.
+      def update_contact(contact, name, street, city, postal_code, country_code, voice, email, ident, ident_type, legal_document, legal_doc_type)
+        builder = build_epp_request do |xml|
+          xml.command {
+            xml.update {
+              xml.update('xmlns:contact' => XML_NS_CONTACT, 'xsi:schemaLocation' => 'http://www.nic.cz/xml/epp/contact-1.6 contact-1.6.xsd') {
+                xml.parent.namespace = xml.parent.namespace_definitions.first
+                xml.id_ contact
+                if [name, street, city, postal_code, country_code].any?{ |item| !item.nil? }
+                  xml.postalInfo {
+                    xml.name name if name
+                    if [street, city, postal_code, country_code].any?{ |item| !item.nil? }
+                      xml.addr {
+                        xml.street street if street
+                        xml.city city if city
+                        xml.pc postal_code if postal_code
+                        xml.cc country_code if country_code
+                      }
+                    end
+                  }
+                end
+                xml.voice voice if voice
+                xml.email email if email
+                xml.ident ident, 'type' => ident_type
+              }
+            }
+            xml.extension {
+              xml.extdata('xmlns:eis' => 'urn:ee:eis:xml:epp:eis-1.0', 'xsi:schemaLocation' => 'urn:ee:eis:xml:epp:eis-1.0 eis-1.0.xsd') {
+                xml.parent.namespace = xml.parent.namespace_definitions.first
+                xml.legalDocument Base64.encode64(legal_document), 'type' => legal_doc_type
+              }
+            }
+            xml.clTRID UUIDTools::UUID.timestamp_create.to_s
+          }
+        end
+        
+        ContactUpdateResponse.new(request(builder.to_xml))
       end
     end
   end
--- spec/lib/epp-eis/contact_spec.rb
+++ spec/lib/epp-eis/contact_spec.rb
@@ -6,7 +6,7 @@ describe Epp::Eis::ContactCommands do
     @server = Epp::Server.new(:server => '127.0.0.1', :tag => 'username', :password => 'password')
   end
   
-  describe 'check_domain' do
+  describe 'check_contact' do
 
     context 'when response is successful' do
 
@@ -22,6 +22,85 @@ describe Epp::Eis::ContactCommands do
         it 'has multiple contact check items in response object' do
           @server.check_contact('CID:FRAKTAL:1', 'CID:FRAKTAL:2').items.size.should == 2
         end
+      end
+    end
+  end
+  
+  describe 'create_contact' do
+    context 'when response is successful' do
+      before(:each) do
+        @server.stub(:request).and_return(xml_mock('responses/create_contact_1000.xml'))
+        @response = @server.create_contact('CID:TEST:10', 'Test', 'Test Street 11-2', 'Test City', '123456', 'EE', '+372.5555555', 'test@test.com', '37812124567', 'op')
+      end
+
+      it 'returns contact id' do
+        @response.contact_id.should == 'CID:TEST:10'
+      end
+
+      it 'returns contact create date' do
+        @response.contact_create_date.should == '2010-02-08T15:50:55+02:00'
+      end
+    end
+  end
+  
+  describe 'info_contact' do
+    context 'when response is successful' do
+      before(:each) do
+        @server.stub(:request).and_return(xml_mock('responses/info_contact_1000.xml'))
+        @response = @server.info_contact('CID:TEST:1')
+      end
+      
+      it 'returns contact id' do
+        @response.contact_id.should == 'CID:TEST:1'
+      end
+
+      it 'returns roid' do
+        @response.contact_roid.should == 'C0000000006-EPP'
+      end
+
+      it 'returns status' do
+        @response.contact_status.should == 'linked'
+      end
+      
+      it 'returns name' do
+        @response.contact_name.should == 'Uus nimi ajee'
+      end
+      
+      it 'returns street' do
+        @response.contact_street.should == 'Kastani'
+      end
+      
+      it 'returns city' do
+        @response.contact_city.should == 'Tallinn'
+      end
+      
+      it 'returns postal code' do
+        @response.contact_postal_code.should == '10613'
+      end
+      
+      it 'returns country code' do
+        @response.contact_country_code.should == 'EE'
+      end
+      
+      it 'returns contact email' do
+        @response.contact_email.should == 'test@test.host'
+      end
+    end
+  end
+  
+  describe 'update_contact' do
+    context 'when response is successful' do
+      before(:each) do
+        @server.stub(:request).and_return(xml_mock('responses/update_contact_1000.xml'))
+        @response = @server.update_contact('CID:TEST:10', nil, nil, nil, nil, nil, nil, nil, nil, 'op', legal_mock('test.pdf'), 'pdf')
+      end
+
+      it 'returns response code' do
+        @response.code.should == 1000
+      end
+
+      it 'returns response message' do
+        @response.message.should == 'Command completed successfully'
       end
     end
   end
